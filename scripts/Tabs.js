ae6c24f060bf8d117b714624d266984bcdbe98b3
PolyakGG <ipoly04@gmail.com>
2025-11-18 22:32:23 +0300

    Rework Tabs functional - add Proxy object and key event listeners

--- scripts/Tabs.js
+++ scripts/Tabs.js
@@ -1,6 +1,8 @@
+import BaseComponent from './BaseComponent.js'
+
 const rootSelector = '[data-js-tabs]'
 
-class Tabs {
+class Tabs extends BaseComponent {
   selectors = {
     root: rootSelector,
     button: '[data-js-tabs-button]',
@@ -17,14 +19,15 @@ class Tabs {
   }
 
   constructor(rootElement) {
+    super()
     this.rootElement = rootElement
     this.buttonElements = this.rootElement.querySelectorAll(this.selectors.button)
     this.contentElements = this.rootElement.querySelectorAll(this.selectors.content)
-    this.state = {
+    this.state = this.getProxyState({
       activeTabIndex: [...this.buttonElements].findIndex(buttonElement =>
         buttonElement.classList.contains(this.stateClasses.isActive)
       ),
-    }
+    })
     this.limitTabsIndex = this.buttonElements.length - 1
     this.bindEvents()
   }
@@ -47,15 +50,69 @@ class Tabs {
     })
   }
 
+  activateTab(newTabIndex) {
+    this.state.activeTabIndex = newTabIndex
+    this.buttonElements[newTabIndex].focus()
+  }
+
+  previousTab = () => {
+    const newTabIndex =
+      this.state.activeTabIndex === 0 ? this.limitTabsIndex : this.state.activeTabIndex - 1
+
+    this.activateTab(newTabIndex)
+  }
+
+  nextTab = () => {
+    const newTabIndex =
+      this.state.activeTabIndex === this.limitTabsIndex ? 0 : this.state.activeTabIndex + 1
+
+    this.activateTab(newTabIndex)
+  }
+
+  firstTab = () => {
+    this.activateTab(0)
+  }
+
+  lastTab = () => {
+    this.activateTab(this.limitTabsIndex)
+  }
+
   onButtonClick(buttonIndex) {
     this.state.activeTabIndex = buttonIndex
-    this.updateUI()
+  }
+
+  onKeyDown = event => {
+    const { code, metaKey } = event
+
+    const action = {
+      ArrowLeft: this.previousTab,
+      ArrowRight: this.nextTab,
+      Home: this.firstTab,
+      End: this.lastTab,
+    }[code]
+
+    const isMacHomeKey = metaKey && code === 'ArrowLeft'
+
+    if (isMacHomeKey) {
+      this.firstTab()
+      return
+    }
+
+    const isMacEndKey = metaKey && code === 'ArrowRight'
+
+    if (isMacEndKey) {
+      this.lastTab()
+      return
+    }
+
+    action?.()
   }
 
   bindEvents() {
     this.buttonElements.forEach((buttonElement, index) => {
       buttonElement.addEventListener('click', () => this.onButtonClick(index))
     })
+    this.rootElement.addEventListener('keydown', this.onKeyDown)
   }
 }
 
